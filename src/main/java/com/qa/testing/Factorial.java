943ad9b537a3724ca05fda57efdafb9e1635bb14
jharry444 <jharrison1@qa.com>
2024-02-08 14:42:57 +0000

    factorial example

---
+++ src/main/java/com/qa/testing/Factorial.java
@@ -0,0 +1,19 @@
+package com.qa.testing;
+
+public class Factorial {
+
+	public String reverse(double num) {
+		int i = 1;
+
+		while (num > 1) {
+			num /= ++i;
+
+			if (num % 1 != 0) {
+				return "NONE";
+			}
+		}
+
+		return i + "!";
+
+	}
+}
